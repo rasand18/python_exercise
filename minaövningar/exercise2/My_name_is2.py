41bfeea054ccf67cc0fd4c7af20e0f4faa45be5d
Rasmus <rasmusandreasson@outlook.com>
2021-06-14 20:01:23 +0200

    Added My_name_is2

---
+++ minaövningar/exercise2/My_name_is2.py
@@ -0,0 +1,9 @@
+
+f_name = input("What's your first name?: ").capitalize()
+l_name = input("What's your last_name?: ").capitalize()
+course = input("Name of your course?: ").lower()
+students = int(input("How many students are there in you class:? "))
+
+
+
+print(f'My name is {f_name}, and my last name is {l_name}.\nI am participating in the course {course}.\nThere are {students} candidates taking the course ')
