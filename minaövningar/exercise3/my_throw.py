6eacb69ef4c268ca92ee0b5c68c1a80bd8ee48d2
Rasmus <rasmusandreasson@outlook.com>
2021-06-15 22:28:48 +0200

    Adding files

--- minaövningar/exercise3/my_throw.py
+++ minaövningar/exercise3/my_throw.py
@@ -6,9 +6,9 @@ throw_height = float(input("Enter throw_height: "))
 
 angle_radians=math.radians(degrees)
 throw_velocity = velocity/3.6
-horz_velocity = velocity*math.cos(angle_radians)
-verti_velocity = velocity*math.sin(angle_radians)
+horz_velocity = throw_velocity*math.cos(angle_radians)
+verti_velocity = throw_velocity*math.sin(angle_radians)
 air_time = (verti_velocity+ math.sqrt(verti_velocity**2+2*9.81*throw_height))/9.81
 throw_distance = horz_velocity*air_time 
 
-print(f"The angle is {angle_radians}\n, the velocity: {throw_velocity}\n, the veolocity horz: {horz_velocity}\n, the velocity verti: {verti_velocity}\n, the air time: {air_time}\n and the throwing distance: {throw_distance}")
+print(f"The angle is {angle_radians:.2f}\n, the velocity: {throw_velocity:.2f}\n, the veolocity horz: {horz_velocity:.2f}\n, the velocity verti: {verti_velocity:.2f}\n, the air time: {air_time:.2f}\n and the throwing distance: {throw_distance:.2f}")
