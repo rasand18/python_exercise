1aab66d1cb14dabe934fd45efb704070299e470a
Rasmus <rasmusandreasson@outlook.com>
2021-06-16 23:23:08 +0200

    Modified bart

--- minaövningar/exercise8/bart_loop_while.py
+++ minaövningar/exercise8/bart_loop_while.py
@@ -1,14 +1,12 @@
-
 #def bart_cheat_code_while(punishment_text):
-    #while bart:
+    #hile bart:
         #print(punishment_text)
 
-#def bart_cheat_code_loop(repititions,punishment_text):
-    #for i in range(repititions):
-       #print(punishment_text)
+def bart_cheat_code_loop(repititions,punishment_text):
+    for i in range(repititions):
+       print(punishment_text)
 
 #bart = True
-#art_cheat_code_while("I will not teach others to fly")
-
-#bart_cheat_code_loop(3,"Jag heter Rasmus")
+#bart_cheat_code_while("I will not teach others to fly")
+bart_cheat_code_loop(3,"I will not teach others to fly")
 
